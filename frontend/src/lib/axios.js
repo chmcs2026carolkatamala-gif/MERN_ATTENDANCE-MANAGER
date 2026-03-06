4286a2dc456c0d7ea256835e25cf96d39eb63f6b
chmcs2026carolkatamala-gif <chmcs2026.carol.katamala@gmail.com>
2026-03-06 23:19:13 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api=axios.create({
-    baseURL:'http://localhost:3000'
-    //baseURL:'https://mern-employee-management-system-wtwk.onrender.com'
+    //baseURL:'http://localhost:3000'
+    baseURL:'https://mern-attendance-manager.onrender.com'
 })
 export default api
